ff464ca38e688e2d052db12aae575f9634a81e74
Maximo Aranibe <darknesswong@gmail.com>
2023-05-02 19:13:29 -0300

    Commit por las dudas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import productRouter from "./routes/products.router.js";
 import cartRouter from "./routes/carts.router.js";
 import realTimeProducts from "./routes/realTimeProducts.router.js";
 import EventEmitter from "events";
+import mongoose from "mongoose";
+
+/* 
+
+  -- Agregar modelo de persistencia en DB utilizando MongoDB y mongoose en tu proyecto.
+
+  -- Crear una base de datos llamada "ecommerce" dentro de tu Atlas, crea sus colecciones "carts", "messages", "product", y sus respectivos schemas.
+
+  -- Agregar managers de productos y carritos pero ahora utilizando MongoDB. NO ELIMINAR managers de FileSystem de tu proyecto.
+
+  -- Separar los managers de fileSystem de los managers de MongoDb en una sola carpeta "dao". dentro de dao, agregar tambien una carpeta "models" donde viviran los esquemas de MongoDB. La estructura debera ser igual a la vista en esta clase.
+
+  -- Reajustar los servicios con el fin de que puedan funcionar con Mongoose en lugar de FileSystem.
+
+  -- Implementar una vista en handlevars llamada chat.handlebars, la cual permitira implementar un chat como el visto en  clase. Los mensajes deberan guardarse en una ocoleccion "messages" en mongo (no es necesario implemntarlo en FileSystem). El formato es [user: correoDelUsuario, message:mensajeDelUsuario].
+
+  -- Corroborar la integridad del proyecto para que todo funcione como lo ha hecho hasta ahora.
+
+*/
+
+
+
 EventEmitter.defaultMaxListeners = 15;
+
+const database = "mongodb+srv://darknesswong:dufONctAnRpA4BNK@mongopruebas.nqjsihc.mongodb.net/test"
+mongoose.connect(database)
+
 
 //Express
 const app = express();
@@ -29,15 +55,5 @@ const httpServer = app.listen(8080, () => {
 });
 
 
-/* 
-const io = new Server(httpServer);
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
- */
 
 export default httpServer; 
